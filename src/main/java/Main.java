7c217d6f32aa49162ddfe4bab584c450fbf964e1
Stanislav Panov <xeqtr2009@yandex.ru>
2021-06-12 07:51:47 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.pow(4,2));
+        System.out.println(calc.mult(45, 2));
+        System.out.println(calc.sum(8, 9));
+    }
+}
